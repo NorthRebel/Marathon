50b1ed2a5fbcf200aa2487fb3ecfdec74d1bfd45
NorthRebel <zatinasever@gmail.com>
2018-10-16 15:29:11 +0300

    Relase base implementation of INotifyPropertyChanged for other View Models

---
+++ Marathon.Desktop/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Marathon.Desktop.Annotations;
+using PropertyChanged;
+
+namespace Marathon.Desktop.ViewModel.Base
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
