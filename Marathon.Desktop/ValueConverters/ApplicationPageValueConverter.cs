d9a147b678c64a828bfd3c932a98db3ad3efd406
NorthRebel <zatinasever@gmail.com>
2018-10-17 19:45:02 +0300

    Add PageHost, which allows comunicate ApplicationViewModel and pages

---
+++ Marathon.Desktop/ValueConverters/ApplicationPageValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using Marathon.Core.Models;
+
+namespace Marathon.Desktop.ValueConverters
+{
+
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
+    /// </summary>
+    class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType = null, object parameter = null, CultureInfo culture = null)
+        {
+            // Find the appropriate page
+            switch ((ApplicationPage)value)
+            {
+                
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
