be3bc94eef1610cf6e9176b1a36b7cc793f7d252
NorthRebel <zatinasever@gmail.com>
2018-10-18 15:29:14 +0300

    Create view model for title bar of other pages, but temporary works only design model

---
+++ Marathon.Core/ViewModel/TitleBar/Design/MainTitleBarDesignModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Marathon.Core.ViewModel.TitleBar.Design
+{
+    /// <summary>
+    /// The design-time data for a <see cref="TitleBarViewModel"/>
+    /// </summary>
+    public class TitleBarDesignModel : TitleBarViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static TitleBarDesignModel Instance => new TitleBarDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TitleBarDesignModel()
+        {
+            LogoutButtonVisible = false;
+        }
+
+        #endregion
+    }
+}
---
+++ Marathon.Core/ViewModel/TitleBar/TitleBarViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+using Marathon.Core.ViewModel.Base;
+
+namespace Marathon.Core.ViewModel.TitleBar
+{
+    /// <summary>
+    /// The view model for a title bar control
+    /// </summary>
+    public class TitleBarViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Hide logout button if user isn't sign in
+        /// </summary>
+        public bool LogoutButtonVisible { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Returns user to previous page
+        /// </summary>
+        public ICommand GoBackCommand { get; set; }
+
+        /// <summary>
+        /// Logout user from system
+        /// </summary>
+        public ICommand LogoutCommand { get; set; }
+
+        #endregion
+    }
+}
