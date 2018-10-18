e1e3c28cf08f3ba30f3acb67b968edbda5a447c2
NorthRebel <zatinasever@gmail.com>
2018-10-18 21:06:53 +0300

    Create page caption control for a lot of pages of application

---
+++ Marathon.Core/ViewModel/PageCaption/PageCaptionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Marathon.Core.ViewModel.Base;
+
+namespace Marathon.Core.ViewModel.PageCaption
+{
+    /// <summary>
+    /// The view model for a page caption control
+    /// </summary>
+    public class PageCaptionViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Caption of a page
+        /// </summary>
+        public string Caption { get; set; }
+
+        /// <summary>
+        /// Description, which explains information about page content 
+        /// </summary>
+        public string Description { get; set; }
+
+        #endregion
+
+    }
+}
