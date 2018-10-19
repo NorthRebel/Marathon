9c32b6e5e480eb1ab507049beeb33c09f8f3c3c0
NorthRebel <zatinasever@gmail.com>
2018-10-19 21:57:38 +0300

    Group controls by type to other folders with adjustment namespaces

---
+++ Marathon.Desktop/Controls/Bars/MainTitleBarControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Marathon.Desktop.Controls.Bars
+{
+    /// <summary>
+    /// Логика взаимодействия для MainTitleBarControl.xaml
+    /// </summary>
+    public partial class MainTitleBarControl : UserControl
+    {
+        public MainTitleBarControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Marathon.Desktop/Controls/Bars/TitleBarControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using Marathon.Core.IoC;
+
+namespace Marathon.Desktop.Controls.Bars
+{
+    /// <summary>
+    /// Логика взаимодействия для TitleBarControl.xaml
+    /// </summary>
+    public partial class TitleBarControl : UserControl
+    {
+        public TitleBarControl()
+        {
+            InitializeComponent();
+
+            // Set data context to Title Bar view model
+            DataContext = IoC.TitleBar;
+        }
+    }
+}
