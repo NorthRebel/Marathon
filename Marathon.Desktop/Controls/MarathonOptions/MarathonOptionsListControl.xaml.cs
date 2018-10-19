c3d9b6879be85800e7d7c755b8f6eed7b26cce69
NorthRebel <zatinasever@gmail.com>
2018-10-20 01:11:01 +0300

    Add dependency property for MarathonOptionsListControl which can select control type to use of included items (not tested)

--- Marathon.Desktop/Controls/MarathonOptions/MarathonOptionsListControl.xaml.cs
+++ Marathon.Desktop/Controls/MarathonOptions/MarathonOptionsListControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Marathon.Desktop.Models;
 
 namespace Marathon.Desktop.Controls.MarathonOptions
 {
@@ -20,9 +22,53 @@ namespace Marathon.Desktop.Controls.MarathonOptions
     /// </summary>
     public partial class MarathonOptionsListControl : UserControl
     {
+        #region Private Members
+
+        private const string RadioGroupName = "OptionsItemsGroup";
+
+        #endregion
+
+        #region Dependency Properties
+        
+        public OptionSelectionType SelectionType    
+        {
+            get => (OptionSelectionType)GetValue(SelectionTypeProperty);
+            set => SetValue(SelectionTypeProperty, value);
+        }
+        
+
+        public static readonly DependencyProperty SelectionTypeProperty =
+            DependencyProperty.Register(nameof(SelectionType), typeof(OptionSelectionType), typeof(MarathonOptionsItemControl), new PropertyMetadata(OptionSelectionType.CheckBox, SelectionTypeCallBack));
+
+
+        #endregion
+
         public MarathonOptionsListControl()
         {
             InitializeComponent();
         }
+
+        #region Dependency Helpers
+
+        private static void SelectionTypeCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var itemControl = d as MarathonOptionsItemControl;
+
+            switch ((OptionSelectionType)e.NewValue)
+            {
+                case OptionSelectionType.CheckBox:
+                    itemControl.RadioButtonControl.GroupName = string.Empty;
+                    itemControl.ClearValue(RadioButton.IsCheckedProperty);
+                    itemControl.RadioButtonControl.Visibility = Visibility.Collapsed;
+                    break;
+                case OptionSelectionType.RadioButton:
+                    itemControl.RadioButtonControl.GroupName = RadioGroupName;
+                    itemControl.ClearValue(CheckBox.IsCheckedProperty);
+                    itemControl.CheckBoxControl.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Marathon.Desktop/Models/OptionSelectionType.cs
@@ -0,0 +1,20 @@
+﻿using Marathon.Desktop.Controls.MarathonOptions;
+
+namespace Marathon.Desktop.Models
+{
+    /// <summary>
+    /// Selection type for a <see cref="MarathonOptionsItemControl"/>
+    /// </summary>
+    public enum OptionSelectionType
+    {
+        /// <summary>
+        /// Select some items of list
+        /// </summary>
+        CheckBox,
+
+        /// <summary>
+        /// Select only one item of list
+        /// </summary>
+        RadioButton
+    }
+}
