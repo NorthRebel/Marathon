e10e7726bd10a1f55ce429e05dc2f9a1e8f9a87e
NorthRebel <zatinasever@gmail.com>
2018-10-20 22:40:55 +0300

    Add UI maanger that hanles any UI interaction in the application

---
+++ Marathon.Core/IoC/Interfaces/IUIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Marathon.Core.ViewModel.Dialogs;
+
+namespace Marathon.Core.IoC.Interfaces
+{
+    /// <summary>
+    /// The UI manager that handles any UI interaction in the application
+    /// </summary>
+    public interface IUIManager
+    {
+        /// <summary>
+        /// Displays a single message box to the user
+        /// </summary>
+        /// <param name="viewModel">The view model</param>
+        /// <returns></returns>
+        Task ShowMessage(MessageBoxDialogViewModel viewModel);
+
+        /// <summary>
+        /// Displays a dialog message about selected charity
+        /// </summary>
+        Task ShowAboutCharityInforamtion(AboutCharityDialogViewModel viewModel);
+    }
+}
---
+++ Marathon.Desktop/IoC/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Marathon.Core.IoC.Interfaces;
+using Marathon.Core.ViewModel.Dialogs;
+using Marathon.Desktop.Dialogs;
+
+namespace Marathon.Desktop.IoC
+{
+    /// <summary>
+    /// The applications implementation of the <see cref="IUIManager"/>
+    /// </summary>
+    public class UIManager : IUIManager
+    {
+        /// <summary>
+        /// Displays a single message box to the user
+        /// </summary>
+        /// <param name="viewModel">The view model</param>
+        /// <returns></returns>
+        public Task ShowMessage(MessageBoxDialogViewModel viewModel)
+        {
+            return new DialogMessageBox().ShowDialog(viewModel);
+        }
+
+        /// <summary>
+        /// Displays a dialog message about selected charity  
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        public Task ShowAboutCharityInforamtion(AboutCharityDialogViewModel viewModel)
+        {
+            return new AboutCharityDialog().ShowDialog(viewModel);
+        }
+    }
+}
