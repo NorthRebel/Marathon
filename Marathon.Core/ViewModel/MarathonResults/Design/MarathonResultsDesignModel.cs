0dfdb222260377f4d2fc039e3f43331e42584dac
NorthRebel <zatinasever@gmail.com>
2018-10-22 14:21:35 +0300

    Create view and design models for marathon results page and controls (not existed yet)

---
+++ Marathon.Core/ViewModel/MarathonResults/Design/MarathonResultsDesignModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using Marathon.Core.ViewModel.MarathonResults.Models;
+using Marathon.Core.ViewModel.MarathonResults.ResultsList.Design;
+
+namespace Marathon.Core.ViewModel.MarathonResults.Design
+{
+    /// <summary>
+    /// The design-time data for a <see cref="MarathonResultsViewModel"/>
+    /// </summary>
+    public class MarathonResultsDesignModel : MarathonResultsViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static MarathonResultsDesignModel Instance => new MarathonResultsDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MarathonResultsDesignModel()
+        {
+            Marathons.Value = new[]
+            {
+                "2014 - Japan",
+                "2015 - Berlin",
+            };
+
+            Distances.Value = new[]
+            {
+                "10 км малый марафон",
+                "42 км полный марафон"
+            };
+
+            Genders.Value = new[]
+            {
+                "Любой",
+                "Мужской",
+                "Женский"
+            };
+
+            AgeCategories.Value = new[]
+            {
+                "18 - 29",
+                "30 - 35",
+                "36 - 45"
+            };
+
+            TotalRunnersCount.Value = 1234;
+            FinishedRunnersCount.Value = 1198;
+            AverageTime.Value = new TimeSpan(4, 2, 10);
+
+            ResultsCondition = new MarathonResultsCondition
+            {
+                Marathon = Marathons.Value.First(),
+                Distance = Distances.Value.First(), 
+                Gender = Genders.Value.First(),
+                AgeCategory = AgeCategories.Value.First()
+            };
+
+            Results = new MarathonResultsListDesignModel();
+        }
+
+        #endregion
+    }
+}
---
+++ Marathon.Core/ViewModel/MarathonResults/MarathonResultsViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections.Generic;
+using Marathon.Core.ViewModel.Base;
+using Marathon.Core.ViewModel.Input;
+using Marathon.Core.ViewModel.PageCaption;
+using Marathon.Core.ViewModel.MarathonResults.Models;
+using Marathon.Core.ViewModel.MarathonResults.ResultsList;
+
+namespace Marathon.Core.ViewModel.MarathonResults
+{
+    /// <summary>
+    /// The view model for a marathon results page
+    /// </summary>
+    public class MarathonResultsViewModel : PageViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// List of spent marathons
+        /// </summary>
+        public EntryViewModel<IEnumerable<string>> Marathons { get; set; }
+
+        /// <summary>
+        /// List of distances of marathon
+        /// </summary>
+        public EntryViewModel<IEnumerable<string>> Distances { get; set; }
+
+        /// <summary>
+        /// List of runner gender
+        /// </summary>
+        public EntryViewModel<IEnumerable<string>> Genders { get; set; }
+
+        /// <summary>
+        /// List of age categories
+        /// </summary>
+        public EntryViewModel<IEnumerable<string>> AgeCategories { get; set; }
+
+        /// <summary>
+        /// Count of runners which take part on marathon
+        /// </summary>
+        public EntryViewModel<long> TotalRunnersCount { get; set; }
+
+        /// <summary>
+        /// Count of runners which complete marathon distance
+        /// </summary>
+        public EntryViewModel<long> FinishedRunnersCount { get; set; }
+
+        /// <summary>
+        /// Average time of completion marathon distance
+        /// </summary>
+        public EntryViewModel<TimeSpan> AverageTime { get; set; }
+
+        /// <inheritdoc cref="MarathonResultsCondition"/>
+        public MarathonResultsCondition ResultsCondition { get; set; }
+
+        /// <inheritdoc cref="MarathonResultsListViewModel"/>
+        public MarathonResultsListViewModel Results { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Find marathon results by condition of marathon properties
+        /// </summary>
+        public ICommand FindCommand { get; set; }
+
+        /// <summary>
+        /// Clear marathon results condition
+        /// </summary>
+        public ICommand ClearCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public MarathonResultsViewModel()
+        {
+            PageCaption = new PageCaptionViewModel
+            {
+                Caption = "Результаты предыдущих гонок"
+            };
+
+            Marathons = new EntryViewModel<IEnumerable<string>>("Марафон");
+            Distances = new EntryViewModel<IEnumerable<string>>("Дистанция");
+            Genders = new EntryViewModel<IEnumerable<string>>("Пол");
+            AgeCategories = new EntryViewModel<IEnumerable<string>>("Категория");
+
+            TotalRunnersCount = new EntryViewModel<long>("Всего бегунов");
+            FinishedRunnersCount = new EntryViewModel<long>("Всего бегунов финишировало");
+            AverageTime = new EntryViewModel<TimeSpan>("Среднее время");
+
+            Results = new MarathonResultsListViewModel();
+            ResultsCondition = new MarathonResultsCondition();
+
+            FindCommand = new RelayCommand(FindMarathonResults);
+            ClearCommand = new RelayCommand(ClearMarathonResults);
+        }
+
+        #endregion
+
+        #region Command Helpers
+
+        private void FindMarathonResults(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ClearMarathonResults(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        
+        #endregion
+    }
+}
