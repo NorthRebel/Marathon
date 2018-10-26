312728bef03ea58d8b51cce887c96bac2d10b4a4
NorthRebel <zatinasever@gmail.com>
2018-10-26 09:49:09 +0300

    Create view and design models for sponsorship review page and inner charity list control (not existed yet both)

---
+++ Marathon.Core/ViewModel/SponsorsReview/SponsorsReviewViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Input;
+using System.Collections.Generic;
+using Marathon.Core.ViewModel.Base;
+using Marathon.Core.ViewModel.Input;
+using Marathon.Core.ViewModel.PageCaption;
+using Marathon.Core.ViewModel.SponsorsReview.CharityList;
+
+namespace Marathon.Core.ViewModel.SponsorsReview
+{
+    /// <summary>
+    /// The view model for a sponsors review page
+    /// </summary>
+    public class SponsorsReviewViewModel : PageViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Attributes of sponsorship organizations for <see cref="SortCommand"/>
+        /// </summary>
+        public EntryViewModel<IEnumerable<string>> AttributesToSort { get; set; }
+
+        /// <inheritdoc cref="CharityListViewModel"/>
+        public CharityListViewModel CharityList { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Sort sponsorship organization by selected attribute
+        /// </summary>
+        public ICommand SortCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public SponsorsReviewViewModel()
+        {
+            PageCaption = new PageCaptionViewModel
+            {
+                Caption = "Просмотр спонсоров"
+            };
+        }
+
+        #endregion
+
+        #region Command Helpers
+
+
+
+        #endregion
+    }
+}
