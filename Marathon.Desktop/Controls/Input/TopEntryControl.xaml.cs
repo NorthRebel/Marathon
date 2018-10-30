14d6642708b847617a67215533a9eb3a59a5bdf6
NorthRebel <zatinasever@gmail.com>
2018-10-30 03:30:02 +0300

    Add InnerContentVerticalAlignment dependency property for TopEntryControl

--- Marathon.Desktop/Controls/Input/TopEntryControl.xaml.cs
+++ Marathon.Desktop/Controls/Input/TopEntryControl.xaml.cs
@@ -20,9 +20,41 @@ namespace Marathon.Desktop.Controls.Input
     /// </summary>
     public partial class TopEntryControl : UserControl
     {
+        #region Dependency Properties
+
+        /// <summary>
+        /// Vertically aligns inner content of control
+        /// </summary>
+        public VerticalAlignment InnerContentVerticalAlignment
+        {
+            get => (VerticalAlignment)GetValue(ContentHorizontalAlignmentProperty);
+            set => SetValue(ContentHorizontalAlignmentProperty, value);
+        }
+
+        public static readonly DependencyProperty ContentHorizontalAlignmentProperty =
+            DependencyProperty.Register(nameof(InnerContentVerticalAlignment), typeof(VerticalAlignment), typeof(TopEntryControl),
+                new PropertyMetadata(VerticalAlignment.Center, VerticalAlignmentPropertyChanged));
+
+        #endregion
+
         public TopEntryControl()
         {
             InitializeComponent();
         }
+
+        #region Dependency Helpers
+
+        private static void VerticalAlignmentPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var entryControl = d as TopEntryControl;
+
+            if (entryControl.ApplyTemplate())
+            {
+                var contentPresenter = entryControl.Template.FindName("InputContent", entryControl) as ContentPresenter;
+                contentPresenter.VerticalAlignment = (VerticalAlignment)e.NewValue;
+            }
+        }
+
+        #endregion
     }
 }
