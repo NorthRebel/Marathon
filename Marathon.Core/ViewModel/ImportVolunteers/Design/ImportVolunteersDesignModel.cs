1d350472937c77641b7fe11a568594a99804e469
NorthRebel <zatinasever@gmail.com>
2018-10-30 06:01:37 +0300

    Create view and design models for import to volunteers page

---
+++ Marathon.Core/ViewModel/ImportVolunteers/Design/ImportVolunteersDesignModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Marathon.Core.ViewModel.Input;
+
+namespace Marathon.Core.ViewModel.ImportVolunteers.Design
+{
+    /// <summary>
+    /// The design-time data for a <see cref="ImportVolunteersViewModel"/>
+    /// </summary>
+    public class ImportVolunteersDesignModel : ImportVolunteersViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static ImportVolunteersDesignModel Instance => new ImportVolunteersDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ImportVolunteersDesignModel()
+        {
+            var attributesRequirements = new List<EntryViewModel<string>>();
+
+            for (int i = 1; i <= 5; i++)
+                attributesRequirements.Add(new EntryViewModel<string>($"Поле {i}") {Value = "Описание поля"});
+
+            RequiredFields = attributesRequirements;
+        }
+
+        #endregion
+    }
+}
---
+++ Marathon.Core/ViewModel/ImportVolunteers/ImportVolunteersViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Marathon.Core.ViewModel.Base;
+using Marathon.Core.ViewModel.Input;
+using Marathon.Core.ViewModel.PageCaption;
+
+namespace Marathon.Core.ViewModel.ImportVolunteers
+{
+    /// <summary>
+    /// The view model for a import volunteers page
+    /// </summary>
+    public class ImportVolunteersViewModel : SignedInViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Selected file name to import
+        /// </summary>
+        public EntryViewModel<string> FileToImport { get; set; }
+
+        /// <summary>
+        /// List of required fields which <see cref="FileToImport"/> must contain
+        /// </summary>
+        public IEnumerable<EntryViewModel<string>> RequiredFields { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Selects file with volunteers info to import 
+        /// </summary>
+        public ICommand SelectFileCommand { get; set; }
+
+        /// <summary>
+        /// Starts importing volunteers from file process
+        /// </summary>
+        public ICommand ImportCommand { get; set; }
+
+        /// <summary>
+        /// Cancel import preparation (goes to previous page)
+        /// </summary>
+        public ICommand CancelCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public ImportVolunteersViewModel()
+        {
+            PageCaption = new PageCaptionViewModel
+            {
+                Caption = "Загрузка волонтеров",
+            };
+
+            FileToImport = new EntryViewModel<string>("CSV файл волонтеров");
+
+            SelectFileCommand = new RelayCommand(SelectFileToImport);
+            ImportCommand = new RelayCommand(ImportVolunteersFromFile);
+            CancelCommand = new RelayCommand(x => IoC.IoC.Application.GoToPreviousPage());
+        }
+
+        #endregion
+
+        #region Command Helpers
+
+        private void ImportVolunteersFromFile(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SelectFileToImport(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
