a59ea523ac0ead44513e490d089a242735c127ae
NorthRebel <zatinasever@gmail.com>
2018-11-03 17:34:13 +0300

    Create view and design models for BMI Calculator page (without scale view model yet)

---
+++ Marathon.Core/ViewModel/BMICalculator/BmiResultViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Marathon.Core.ViewModel.Base;
+
+namespace Marathon.Core.ViewModel.BMICalculator
+{
+    /// <summary>
+    /// The view model for show body mass index result of calculations
+    /// </summary>
+    public class BMIResultViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Value of BMI calculations
+        /// </summary>
+        public double? Value { get; set; }
+
+        /// <summary>
+        /// Image about user body state
+        /// </summary>
+        public byte[] Image { get; set; }
+
+        /// <summary>
+        /// Description about user body state
+        /// </summary>
+        public string Description { get; set; }
+
+        #endregion
+    }
+}
---
+++ Marathon.Core/ViewModel/BMICalculator/Design/BMICalculatorDesignModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Marathon.Core.ViewModel.BMICalculator.Design
+{
+    /// <summary>
+    /// The design-time data for a <see cref="BMICalculatorViewModel"/>
+    /// </summary>
+    public class BMICalculatorDesignModel : BMICalculatorViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static BMICalculatorDesignModel Instance => new BMICalculatorDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BMICalculatorDesignModel()
+        {
+            Growth.Value = 170;
+            Weight.Value = 70;
+
+            Result = new BMIResultDesignModel();
+        }
+
+        #endregion
+    }
+}
