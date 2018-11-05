32bd630a46fd75a5919423eea90b87dd4c11bb3a
NorthRebel <zatinasever@gmail.com>
2018-11-05 18:05:21 +0300

    Create view and design models for inventrory page (awkward solution)

---
+++ Marathon.Core/ViewModel/Inventory/InventoryList/Design/InventoryListDesignModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace Marathon.Core.ViewModel.Inventory.InventoryList.Design
+{
+    /// <summary>
+    /// The design-time data for a <see cref="InventoryListViewModel"/>
+    /// </summary>
+    public class InventoryListDesignModel : InventoryListViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static InventoryListDesignModel Instance => new InventoryListDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public InventoryListDesignModel()
+        {
+            SignedUpRunnersCount = 123;
+
+            Items = Enumerable.Range(1, 10).Select(x => new InventoryListItemDesignModel());
+
+            var items = Items as InventoryListItemViewModel[] ?? Items.ToArray();
+
+            ItemsSummary = new InventoryListItemViewModel
+            {
+                Name = "Выбрало данный вариант",
+                TypeA = items.Sum(x => x.TypeA),
+                TypeB = items.Sum(x => x.TypeB),
+                TypeC = items.Sum(x => x.TypeC),
+                RequiredCount = items.Sum(x => x.RequiredCount),
+                RemindCount = items.Sum(x => x.RemindCount),
+            };
+        }
+
+        #endregion
+    }
+}
---
+++ Marathon.Core/ViewModel/Inventory/InventoryViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows.Input;
+using Marathon.Core.Models;
+using Marathon.Core.ViewModel.Base;
+using Marathon.Core.ViewModel.Inventory.InventoryList;
+using Marathon.Core.ViewModel.PageCaption;
+
+namespace Marathon.Core.ViewModel.Inventory
+{
+    /// <summary>
+    /// The view model for a inventory page
+    /// </summary>
+    public class InventoryViewModel : SignedInViewModel
+    {
+        #region Public Properties
+
+        /// <inheritdoc cref="InventoryListViewModel"/>
+        public InventoryListViewModel InventoryItems { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Goes to print-preview dialog which shows count of items to order
+        /// </summary>
+        public ICommand ReportCommand { get; set; }
+
+        /// <summary>
+        /// Goes to page which user can replenish inventory items
+        /// </summary>
+        public ICommand ReceiptCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public InventoryViewModel()
+        {
+            PageCaption = new PageCaptionViewModel
+            {
+                Caption = "Инвентарь"
+            };
+
+            InventoryItems = new InventoryListViewModel();
+
+            ReportCommand = new RelayCommand(ShowPrintPreviewReport);
+            ReceiptCommand = new RelayCommand(x => GoToPage(ApplicationPage.InventoryArrive));
+        }
+
+
+        #endregion
+
+        #region Command Helpers
+
+        private void ShowPrintPreviewReport(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+
+        #endregion
+    }
+}
