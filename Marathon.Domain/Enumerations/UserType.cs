9ee22203e12d108160d7fa8ce6bbbce44a33304a
NorthRebel <zatinasever@gmail.com>
2018-11-16 12:36:03 +0300

    Create linked enumeration for UserType entity

---
+++ Marathon.Domain/Enumerations/UserType.cs
@@ -0,0 +1,34 @@
+﻿namespace Marathon.Domain.Enumerations
+{
+    public class UserType : Enumeration
+    {
+        public static UserType Administrator = new AdministratorUserType();
+        public static UserType Coordinator = new CoordinatorUserType();
+        public static UserType Runner = new RunnerUserType();
+
+        public UserType(long id, string name) : base(id, name)
+        {
+        }
+
+        private class AdministratorUserType : UserType
+        {
+            public AdministratorUserType() : base(1, "Administrator")
+            {
+            }
+        }
+
+        private class CoordinatorUserType : UserType
+        {
+            public CoordinatorUserType() : base(2, "Coordinator")
+            {
+            }
+        }
+
+        private class RunnerUserType : UserType
+        {
+            public RunnerUserType() : base(3, "Runner")
+            {
+            }
+        }
+    }
+}
