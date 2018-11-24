e166588eed25f664e97dca258b3b6e7f422f0dee
NorthRebel <zatinasever@gmail.com>
2018-11-25 01:02:57 +0300

    Create linked enumeration for Gender entity

---
+++ Marathon.Domain/Enumerations/Gender.cs
@@ -0,0 +1,26 @@
+﻿namespace Marathon.Domain.Enumerations
+{
+    public class Gender : Enumeration
+    {
+        public static Gender Male = new MaleGender();
+        public static Gender Female = new FemaleGender();
+
+        public Gender(long id, string name) : base(id, name)
+        {
+        }
+
+        private class MaleGender : Gender
+        {
+            public MaleGender() : base(1, "Male")
+            {
+            }
+        }
+
+        private class FemaleGender : Gender
+        {
+            public FemaleGender() : base(2, "Female")
+            {
+            }
+        }
+    }
+}
