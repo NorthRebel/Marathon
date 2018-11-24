a5549b81fd53028c46c545bc85a1c8cb58529452
NorthRebel <zatinasever@gmail.com>
2018-11-25 01:41:45 +0300

    Add abstract factory methods for create IUnitOfWork

---
+++ Marathon.DAL/UnitOfWork/IUnitOfWorkFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace Marathon.DAL.UnitOfWork
+{
+    /// <summary>
+    /// Factory methods for create <see cref="IUnitOfWork"/>
+    /// </summary>
+    public interface IUnitOfWorkFactory
+    {
+        /// <summary>
+        /// Creates instance of <see cref="IUnitOfWork"/> with specific database isolation level
+        /// </summary>
+        /// <param name="isolationLevel">The database isolation level with which the underlying store transaction will be created</param>
+        /// <returns>Instance of <see cref="IUnitOfWork"/></returns>
+        IUnitOfWork Create(IsolationLevel isolationLevel);
+
+        /// <summary>
+        /// Creates instance of <see cref="IUnitOfWork"/> with default value of <see cref="IsolationLevel"/> (ReadCommited)
+        /// </summary>
+        /// <returns>Instance of <see cref="IUnitOfWork"/></returns>
+        IUnitOfWork Create();
+    }
+}
