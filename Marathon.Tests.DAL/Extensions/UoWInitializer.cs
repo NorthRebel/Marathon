da9753a1523b642d9ad351d441eda5a76c959178
NorthRebel <zatinasever@gmail.com>
2018-11-29 09:54:57 +0300

    Remove thread-safe UoW initializer (useless stuff)

--- Marathon.Tests.DAL/Extensions/UoWInitializer.cs
+++ Marathon.Tests.DAL/Extensions/UoWInitializer.cs
@@ -4,28 +4,11 @@ using Marathon.DAL.UnitOfWork;
 
 namespace Marathon.Tests.DAL.Extensions
 {
-    /// <summary>
-    /// Thread-safe unitOfWork initializer
-    /// </summary>
     public static class UoWInitializer
     {
-        private static object _lock = new object();
-        private static bool _initialized;
-
         public static void Initialize(this IUnitOfWork context, Func<IUnitOfWork, Task> seedData)
         {
-            if (!_initialized)
-            {
-                lock (_lock)
-                {
-                    if (_initialized)
-                        return;
-
-                    _initialized = true;
-
-                    seedData(context);
-                }
-            }
+            seedData(context);
         }
     }
 }
