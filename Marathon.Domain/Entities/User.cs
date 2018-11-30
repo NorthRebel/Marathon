c3e2886d7ea275f3a707dd75f6acea49e9647931
NorthRebel <zatinasever@gmail.com>
2018-11-30 09:22:21 +0300

    Replace Runners collection navigation property to single object on User entity for use one-to-one relationship between Runner

--- Marathon.Domain/Entities/User.cs
+++ Marathon.Domain/Entities/User.cs
@@ -8,11 +8,6 @@ namespace Marathon.Domain.Entities
     /// </summary>
     public sealed class User : IEntity<long>
     {
-        public User()
-        {
-            Runners = new HashSet<Runner>();
-        }
-
         public long Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
@@ -21,6 +16,6 @@ namespace Marathon.Domain.Entities
         public char UserTypeId { get; set; }
 
         public UserType UserType { get; set; }
-        public ICollection<Runner> Runners { get; set; }
+        public Runner Runner { get; set; }
     }
 }
