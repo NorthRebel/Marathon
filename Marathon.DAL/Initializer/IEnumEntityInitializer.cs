ff0b340a01b2f96120284c3e92efa7d289fc4169
NorthRebel <zatinasever@gmail.com>
2018-12-01 13:03:21 +0300

    Create initializer for repositories of IUnitOfWork (temporary approach, need to refactor, just only linked enums)

---
+++ Marathon.DAL/Initializer/IEnumEntityInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Marathon.DAL.Repositories;
+using Marathon.Domain.Enumerations;
+
+namespace Marathon.DAL.Initializer
+{
+    using Domain.Common;
+    using Domain.Entities;
+
+    /// <summary>
+    /// List of methods which should initialize repositories of entities which implement <see cref="IEnumEntity{TKey}"/>
+    /// </summary>
+    internal interface IEnumEntityInitializer
+    {
+        /// <summary>
+        /// Seeds <see cref="UserType"/> repository
+        /// </summary>
+        void SeedUserTypes();
+
+        /// <summary>
+        /// Seeds <see cref="Gender"/> repository
+        /// </summary>
+        void SeedGenders();
+
+        /// <summary>
+        /// Seeds <see cref="SignUpStatus"/> repository
+        /// </summary>
+        void SeedSignUpStatuses();
+
+        /// <summary>
+        /// Seed repository with enum entities from defined enum
+        /// </summary>
+        /// <typeparam name="TEntity">Type of repository</typeparam>
+        /// <typeparam name="TKey">Key field type of entity</typeparam>
+        /// <typeparam name="TEntityEnum">Type of linked enumeration</typeparam>
+        /// <param name="repository">Repository to initialize</param>
+        /// <param name="items">Linked enumeration items</param>
+        void SeedEnumeration<TEntity, TKey, TEntityEnum>(IReadOnlyRepository<TEntity> repository, IEnumerable<TEntityEnum> items)
+            where TEntity : IEnumEntity<TKey>, new() where TEntityEnum : Enumeration<TKey, TEntity>;
+    }
+}
---
+++ Marathon.DAL/Initializer/UnitOfWorkInitializer.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Marathon.Domain.Common;
+using Marathon.DAL.UnitOfWork;
+using Marathon.DAL.Repositories;
+using System.Collections.Generic;
+using Marathon.Domain.Enumerations;
+
+namespace Marathon.DAL.Initializer
+{
+    using Gender = Domain.Entities.Gender;
+    using SignUpStatus = Domain.Entities.SignUpStatus;
+    using UserType = Domain.Entities.UserType;
+    using GenderEnum = Domain.Enumerations.Gender;
+    using UserTypeEnum = Domain.Enumerations.UserType;
+    using SignUpStatusEnum = Domain.Enumerations.SignUpStatus;
+
+    /// <summary>
+    /// Initializer for repositories of <see cref="IUnitOfWork"/>
+    /// </summary>
+    public class UnitOfWorkInitializer : IEnumEntityInitializer
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly CancellationToken _cancellationToken;
+
+        public UnitOfWorkInitializer(IUnitOfWork uow, CancellationToken cancellationToken)
+        {
+            _uow = uow;
+            _cancellationToken = cancellationToken;
+        }
+
+        public async Task Seed()
+        {
+            SeedGenders();
+            SeedUserTypes();
+            SeedSignUpStatuses();
+
+            await _uow.CommitAsync(_cancellationToken);
+        }
+
+        #region Initialize linked enumerations
+
+        public void SeedUserTypes()
+        {
+            SeedEnumeration<UserType, char, UserTypeEnum>(_uow.UserTypes, UserTypeEnum.GetAll<UserTypeEnum>());
+        }
+
+        public void SeedGenders()
+        {
+            SeedEnumeration<Gender, char, GenderEnum>(_uow.Genders, GenderEnum.GetAll<GenderEnum>());
+        }
+
+        public void SeedSignUpStatuses()
+        {
+            SeedEnumeration<SignUpStatus, byte, SignUpStatusEnum>(_uow.SignUpStatuses, SignUpStatusEnum.GetAll<SignUpStatusEnum>());
+        }
+
+        #region Seed Helpers        
+
+        public void SeedEnumeration<TEntity, TKey, TEntityEnum>(IReadOnlyRepository<TEntity> repository, IEnumerable<TEntityEnum> items) 
+            where TEntity : IEnumEntity<TKey>, new() where TEntityEnum : Enumeration<TKey, TEntity>
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
