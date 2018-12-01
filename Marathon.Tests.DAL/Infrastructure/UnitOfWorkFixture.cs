14c29872333036a3b9dc5692ed63207a0a5f3499
NorthRebel <zatinasever@gmail.com>
2018-12-01 20:26:38 +0300

    Refactoring data access layer for unit tests to use persistence implementation of DAL layer

--- Marathon.Tests.DAL/Infrastructure/UnitOfWorkFixture.cs
+++ Marathon.Tests.DAL/Infrastructure/UnitOfWorkFixture.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Marathon.DAL.Initializer;
+using Marathon.Persistence;
 using Marathon.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
 
 namespace Marathon.Tests.DAL.Infrastructure
 {
@@ -14,15 +19,47 @@ namespace Marathon.Tests.DAL.Infrastructure
 
         public UnitOfWorkFixture()
         {
-            var databaseName = Guid.NewGuid().ToString();
-
-            ContextFactory = new UnitOfWorkFactory(databaseName);
+            ContextFactory = new UnitOfWorkFactory(GetDbContext());
             Context = ContextFactory.Create();
+
+            Task.Run(() => InitializeContext(Context));
+        }
+
+        private async Task InitializeContext(IUnitOfWork uow)
+        {
+            var initializer = new UnitOfWorkInitializer(uow, CancellationToken.None);
+            await initializer.Seed();
+        }
+
+        private DbContext GetDbContext(bool useSqlLite = false) 
+        {
+            var context = new MarathonDbContext(ConfigureDbContext<MarathonDbContext>(useSqlLite));
+
+            // SQLite needs to open connection to the DB.
+            if (useSqlLite)
+                context.Database.OpenConnection();
+
+            return context;
+        }
+
+        private DbContextOptions<TContext> ConfigureDbContext<TContext>(bool useSqlLite) where TContext : DbContext
+        {
+            var builder = new DbContextOptionsBuilder<TContext>();
+
+            if (useSqlLite)
+                builder.UseSqlite("DataSource=:memory:", x => { });
+            else
+                builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            return builder.Options;
         }
 
         public void Dispose()
         {
-            UnitOfWorkFactory.Destroy(Context);
+            var dbContext = Context as DbContext;
+            dbContext?.Database.EnsureDeleted();
+
+            Context.Dispose();
         }
     }
 }
