80f6a3aefad875e4d0cfd50df973e4c2fd0274f6
NorthRebel <zatinasever@gmail.com>
2018-12-07 18:48:16 +0300

    Fix access modifier for each context entity

--- Marathon.Persistence/MarathonDbContext.cs
+++ Marathon.Persistence/MarathonDbContext.cs
@@ -12,39 +12,39 @@ namespace Marathon.Persistence
     {
         #region Entities
 
-        DbSet<UserType> UserTypes { get; set; }
+        public DbSet<UserType> UserTypes { get; set; }
 
-        DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
 
-        DbSet<Runner> Runners { get; set; }
+        public DbSet<Runner> Runners { get; set; }
 
-        DbSet<Gender> Genders { get; set; }
+        public DbSet<Gender> Genders { get; set; }
 
-        DbSet<Volunteer> Volunteers { get; set; }
+        public DbSet<Volunteer> Volunteers { get; set; }
 
-        DbSet<Country> Countries { get; set; }
+        public DbSet<Country> Countries { get; set; }
 
-        DbSet<MarathonSignUp> MarathonSignUps { get; set; }
+        public DbSet<MarathonSignUp> MarathonSignUps { get; set; }
 
-        DbSet<SignUpStatus> SignUpStatuses { get; set; }
+        public DbSet<SignUpStatus> SignUpStatuses { get; set; }
 
-        DbSet<RaceKitOption> RaceKitOptions { get; set; }
+        public DbSet<RaceKitOption> RaceKitOptions { get; set; }
 
-        DbSet<RaceKitItem> RaceKitItems { get; set; }
+        public DbSet<RaceKitItem> RaceKitItems { get; set; }
 
-        DbSet<RaceKitOptionItem> RaceKitOptionItems { get; set; }
+        public DbSet<RaceKitOptionItem> RaceKitOptionItems { get; set; }
 
-        DbSet<Charity> Charities { get; set; }
+        public DbSet<Charity> Charities { get; set; }
 
-        DbSet<Sponsorship> Sponsorships { get; set; }
+        public DbSet<Sponsorship> Sponsorships { get; set; }
 
-        DbSet<SignUpMarathonEvent> SignUpMarathonEvents { get; set; }
+        public DbSet<SignUpMarathonEvent> SignUpMarathonEvents { get; set; }
 
-        DbSet<Event> Events { get; set; }
+        public DbSet<Event> Events { get; set; }
 
-        DbSet<EventType> EventTypes { get; set; }
+        public DbSet<EventType> EventTypes { get; set; }
 
-        DbSet<Marathon> Marathons { get; set; }
+        public DbSet<Marathon> Marathons { get; set; }
 
         #endregion
 
