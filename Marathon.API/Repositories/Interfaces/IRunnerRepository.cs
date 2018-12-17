fd7b4ba8b808ebc4af4cd59ae50cbedff9e1131c
NorthRebel <zatinasever@gmail.com>
2018-12-17 09:37:09 +0300

    Implement runner sign up functionality in server side

---
+++ Marathon.API/Repositories/Interfaces/IRunnerRepository.cs
@@ -0,0 +1,9 @@
+﻿using Marathon.API.Models.Runner;
+
+namespace Marathon.API.Repositories.Interfaces
+{
+    public interface IRunnerRepository
+    {
+        uint SignUp(RunnerSignUpCredentials credentials);
+    }
+}
