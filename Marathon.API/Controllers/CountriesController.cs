c51b10b21da6f1f232ac99ae95ac5ba6722bc3a3
NorthRebel <zatinasever@gmail.com>
2018-12-17 17:27:39 +0300

    Create countries controller

---
+++ Marathon.API/Controllers/CountriesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Marathon.API.Authentication;
+using Marathon.API.Models.Country;
+using Marathon.API.Repositories.Interfaces;
+
+namespace Marathon.API.Controllers
+{
+    [AuthorizeToken]
+    [Route("[controller]")]
+    public class CountriesController : Controller
+    {
+        private readonly ICountryRepository _countryRepository;
+
+        public CountriesController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        [HttpGet]
+        [Route("All")]
+        [ProducesResponseType(typeof(Countries), (int)HttpStatusCode.OK)]
+        public IActionResult GetAll()
+        {
+            var countries = _countryRepository.GetAllNames();
+
+            return Ok(countries);
+        }
+    }
+}
---
+++ Marathon.API/Repositories/CountryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Marathon.Persistence;
+using Marathon.API.Repositories.Interfaces;
+
+namespace Marathon.API.Repositories
+{
+    internal class CountryRepository : ICountryRepository
+    {
+        private readonly MarathonDbContext _context;
+
+        public CountryRepository(MarathonDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<string> GetAllNames()
+        {
+            return _context.Countries.Select(c => c.Name);
+        }
+    }
+}
