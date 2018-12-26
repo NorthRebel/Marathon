57bfcc03894f8a7ed183b55df6022967efcde224
NorthRebel <zatinasever@gmail.com>
2018-12-27 01:39:05 +0300

    Create and refactor marathon options controls

---
+++ Marathon.Desktop/Controls/Custom/ListBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Collections;
+
+namespace Marathon.Desktop.Controls.Custom
+{
+    /// <summary>
+    /// Custom version of listbox that allows to bind to list of selected items
+    /// </summary>
+    public class ListBox : System.Windows.Controls.ListBox
+    {
+        public ListBox()
+        {
+            SelectionChanged += ListBox_SelectionChanged;
+        }
+
+        private void ListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            SelectedItemsList = SelectedItems;
+        }
+
+        public IList SelectedItemsList
+        {
+            get => (IList)GetValue(SelectedItemsListProperty);
+            set => SetValue(SelectedItemsListProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemsListProperty =
+            DependencyProperty.Register("SelectedItemsList", typeof(IList), typeof(ListBox), new PropertyMetadata(null));
+    }
+}
---
+++ Marathon.Desktop/Controls/MarathonOptions/MarathonOptionsSingle.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Collections;
+using System.Windows.Controls;
+
+namespace Marathon.Desktop.Controls.MarathonOptions
+{
+    /// <summary>
+    /// Interaction logic for MarathonOptionsSingle.xaml
+    /// </summary>
+    public partial class MarathonOptionsSingle : UserControl
+    {
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(nameof(ItemsSource), typeof(IEnumerable), typeof(MarathonOptionsSingle),
+                new FrameworkPropertyMetadata(default(IEnumerable)));
+
+        public object SelectedItem
+        {
+            get => (object)GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(nameof(SelectedItem), typeof(object), typeof(MarathonOptionsSingle),
+                new FrameworkPropertyMetadata(default(object), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+        public MarathonOptionsSingle()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
