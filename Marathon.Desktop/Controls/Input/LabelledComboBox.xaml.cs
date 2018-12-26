cc0c0a9a9ef64e1d16692dc007c60b9fd2502c27
NorthRebel <zatinasever@gmail.com>
2018-12-26 21:39:59 +0300

    Create custom labelled combobox and dateTimePicker controls

---
+++ Marathon.Desktop/Controls/Input/LabelledComboBox.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Collections;
+using System.Windows.Controls;
+
+namespace Marathon.Desktop.Controls.Input
+{
+    /// <summary>
+    /// Interaction logic for LabelledComboBox.xaml
+    /// </summary>
+    public partial class LabelledComboBox : UserControl
+    {
+        #region Dependency Properties
+
+        #region Label
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(nameof(Label), typeof(string), typeof(LabelledComboBox),
+                new FrameworkPropertyMetadata("Unnamed Label"));
+
+        #endregion
+
+        #region ComboBox
+
+        public IEnumerable Items
+        {
+            get => (IEnumerable)GetValue(ItemsProperty);
+            set => SetValue(ItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register(nameof(Items), typeof(IEnumerable), typeof(LabelledComboBox),
+                new FrameworkPropertyMetadata(default(IEnumerable)));
+
+        public object SelectedItem
+        {
+            get => (object)GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(nameof(SelectedItem), typeof(object), typeof(LabelledComboBox),
+                new FrameworkPropertyMetadata(default(object), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        #endregion
+
+        #endregion
+
+        public LabelledComboBox()
+        {
+            InitializeComponent();
+            Root.DataContext = this;
+        }
+    }
+}
---
+++ Marathon.Desktop/Controls/Input/LabelledDatePicker.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Marathon.Desktop.Controls.Input
+{
+    /// <summary>
+    /// Interaction logic for LabelledDatePicker.xaml
+    /// </summary>
+    public partial class LabelledDatePicker : UserControl
+    {
+        #region Dependency Properties
+
+        #region Label
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(nameof(Label), typeof(string), typeof(LabelledDatePicker),
+                new FrameworkPropertyMetadata("Unnamed Label"));
+
+        #endregion
+
+        #region DatePicker
+        
+        public DateTime? Date
+        {
+            get => (DateTime?)GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        }
+
+        public static readonly DependencyProperty DateProperty =
+            DependencyProperty.Register(nameof(Date), typeof(DateTime?), typeof(LabelledDatePicker), 
+                new FrameworkPropertyMetadata(default(DateTime?), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        #endregion
+
+        #endregion
+
+        public LabelledDatePicker()
+        {
+            InitializeComponent();
+            Root.DataContext = this;
+        }
+    }
+}
