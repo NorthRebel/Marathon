ff80a7c804a5330479fc405ac0f6f886d0395bd9
NorthRebel <zatinasever@gmail.com>
2018-12-26 17:31:27 +0300

    Create labelled input controls (now is TestBox and PasswordBox) which will replace left and top entry controls of desktop application

---
+++ Marathon.Desktop/Controls/Input/LabelledPasswordBox.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Marathon.Desktop.Controls.Input
+{
+    /// <summary>
+    /// Interaction logic for LabelledPasswordBox.xaml
+    /// </summary>
+    public partial class LabelledPasswordBox : UserControl
+    {
+        #region Dependency Properties
+
+        #region Label
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(nameof(Label), typeof(string), typeof(LabelledPasswordBox),
+                new FrameworkPropertyMetadata("Unnamed Label"));
+
+        #endregion
+
+        #region Password
+
+        public string PasswordPlaceholder
+        {
+            get => (string)GetValue(PasswordPlaceholderProperty);
+            set => SetValue(PasswordPlaceholderProperty, value);
+        }
+
+        public static readonly DependencyProperty PasswordPlaceholderProperty =
+            DependencyProperty.Register(nameof(PasswordPlaceholder), typeof(string), typeof(LabelledPasswordBox),
+                new PropertyMetadata(default(object), PasswordPlaceholderChanged));
+
+        #endregion
+
+        #endregion
+
+        public LabelledPasswordBox()
+        {
+            InitializeComponent();
+            Root.DataContext = this;
+        }
+
+        #region Dependency properties Helpers
+
+        private static void PasswordPlaceholderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is LabelledPasswordBox ctrl)
+            {
+                ctrl.PasswordText.Tag = e.NewValue;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Marathon.Desktop/Controls/Input/LabelledTextBox.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Marathon.Desktop.Controls.Input
+{
+    /// <summary>
+    /// Interaction logic for LabelledTextBox.xaml
+    /// </summary>
+    public partial class LabelledTextBox : UserControl
+    {
+        #region Dependency Properties
+
+        #region Label
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(nameof(Label), typeof(string), typeof(LabelledTextBox),
+                new FrameworkPropertyMetadata("Unnamed Label"));
+        
+        #endregion
+
+        #region Text
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(LabelledTextBox),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        
+        public string TextPlaceholder
+        {
+            get => (string)GetValue(TextPlaceholderProperty);
+            set => SetValue(TextPlaceholderProperty, value);
+        }
+
+        public static readonly DependencyProperty TextPlaceholderProperty =
+            DependencyProperty.Register(nameof(TextPlaceholder), typeof(string), typeof(LabelledTextBox), 
+                new PropertyMetadata(default(object), TextPlaceholderChanged));
+
+        #endregion
+
+        #endregion
+
+        public LabelledTextBox()
+        {
+            InitializeComponent();
+            Root.DataContext = this;
+        }
+
+        #region Dependency properties Helpers
+
+        private static void TextPlaceholderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is LabelledTextBox ctrl)
+            {
+                ctrl.EntryText.Tag = e.NewValue;
+            }
+        }
+
+        #endregion
+    }
+}
