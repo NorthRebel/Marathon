8b14a85e1953d35962b59f32ae545f12b47710a8
NorthRebel <zatinasever@gmail.com>
2019-01-05 20:40:07 +0300

    Remove template examples from all web projects

--- Marathon.SPA/ClientApp/src/App.js
+++ Marathon.SPA/ClientApp/src/App.js
@@ -1,14 +1,8 @@
 ﻿import React from 'react';
 import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
 
-export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
+const App = () => (
+  <h1>Marathon SKills 2019</h1>
 );
+
+export default App;
